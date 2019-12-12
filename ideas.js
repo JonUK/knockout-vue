ff4f04d50cdd46727e04bd008352854a42664347
JonUK <jonuk2018@jjwebdesign.co.uk>
2019-12-12 01:52:22 +0000

    Changes to update the Knockout todos list from React

--- ideas.js
+++ ideas.js
@@ -31,15 +31,40 @@ var ToDoList = React.createClass({
     mixins: [ KnockoutMixin ],
 
     propTypes: {
-        todos: React.PropTypes.array.isRequired
+        todos: React.PropTypes.array.isRequired,
+        handleUpdate: React.PropTypes.func
     },
 
+    componentDidMount: function () {
+        setInterval(() => {
+            // debugger;
+
+            const localTodos = [...this.props.todos];
+            localTodos.shift();
+            localTodos.push("Added in React context - " + Math.random());
+
+            if (this.props.handleUpdate) {
+                this.props.handleUpdate(localTodos);
+            }
+
+        }, 3000);
+    },
+
+
     render() {
-        return (
-            <ul data-bind="foreach: props.todos">
-                <li data-bind="text: $data"></li>
-            </ul>
-        );
+        // return (
+        //     <ul data-bind="foreach: props.todos">
+        //         <li data-bind="text: $data"></li>
+        //     </ul>
+        // );
+
+        // Converted at: https://babeljs.io/repl/#?browsers=&build=&builtIns=false&spec=false&loose=false&code_lz=GYVwdgxgLglg9mABACwKYBt1wBQEpEDeAUIogE6pQhlLYmkMA8I6iAJgIZQcC0ARjDBsAvACJgcChwjIAXIgAOZOAoDOAOihw2cVaIB89BscSN0Mdl14ChYqKgAeUeQBJO3A4wD05wydNeLH6IuADcRAC-REA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=react&prettier=false&targets=&version=7.7.5&externalPlugins=
+
+        return React.createElement("div", null, React.createElement("h2", null, "Rendered as React component"), React.createElement("ul", {
+            "data-bind": "foreach: props.todos"
+        }, React.createElement("li", {
+            "data-bind": "text: $data"
+        })));
     }
 });
 
